2f5db0c501343345dc5c89c06acb4ecc1bd11514
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-14 21:22:49 +0530

    calling Check line using Equals method

--- Program.cs
+++ Program.cs
@@ -5,10 +5,10 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welsome to Line Comparison Computation Program");
-            Length l = new Length();
-            l.CalLength();
-            //EqualsMethod equalsMethod = new EqualsMethod();
-            //equalsMethod.CompareLinesWithEquals();
+            //Length l = new Length();
+            //l.CalLength();
+            EqualsMethod equalsMethod = new EqualsMethod();
+            equalsMethod.CompareLinesWithEquals();
             // CompareToMethod compareToMethod = new CompareToMethod();
             //compareToMethod.CompareLinesWithCompareTo();
 
