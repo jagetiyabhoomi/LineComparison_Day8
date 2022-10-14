d00e53743a1cf1f403f077c4faf32cd3075e6f6f
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-14 21:14:39 +0530

    Greeting Msg

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LineComparisonProblem
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welsome to Line Comparison Computation Program");
+            //Length l = new Length();
+            //l.CalLength();
+            //EqualsMethod equalsMethod = new EqualsMethod();
+            //equalsMethod.CompareLinesWithEquals();
+            // CompareToMethod compareToMethod = new CompareToMethod();
+            //compareToMethod.CompareLinesWithCompareTo();
+
+        }
+    }
+}
