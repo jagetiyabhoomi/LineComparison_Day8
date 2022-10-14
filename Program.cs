461b6d9f9c01699d8481c0de5959c952751230ff
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-14 21:18:38 +0530

    call method to Calculate Length

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welsome to Line Comparison Computation Program");
-            //Length l = new Length();
-            //l.CalLength();
+            Length l = new Length();
+            l.CalLength();
             //EqualsMethod equalsMethod = new EqualsMethod();
             //equalsMethod.CompareLinesWithEquals();
             // CompareToMethod compareToMethod = new CompareToMethod();
